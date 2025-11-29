8d1bf1679e569db532aef792b814b43ec1cd2b49
Puffinbird <l.noren.93@gmail.com>
2025-11-29 10:15:13 +0100

    ddd main miss

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def setup_hook():
