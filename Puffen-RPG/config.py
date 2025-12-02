e6a5fb7a3a91d616d4a1ad68cee7e1dda4407e4a
Puffinbird <l.noren.93@gmail.com>
2025-12-02 22:33:55 +0100

    conditions i config

--- Puffen-RPG/config.py
+++ Puffen-RPG/config.py
@@ -14,8 +14,28 @@ TOKEN = os.getenv('DISCORD_TOKEN', 'DIN_BOT_TOKEN_HÄR')
 PREFIX = '!'
 
 # Data folder
-DATA_FOLDER = './data'
-DATA_FILE = f'{DATA_FOLDER}/dnd_data.json'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_FOLDER = os.path.join(BASE_DIR, "data")
+DATA_FILE = os.path.join(DATA_FOLDER, "dnd_data.json")
 
 # Skapa data folder om den inte finns
 os.makedirs(DATA_FOLDER, exist_ok=True)
+
+# D&D 5e Conditions
+CONDITIONS = [
+    "Blinded",
+    "Charmed",
+    "Deafened",
+    "Frightened",
+    "Grappled",
+    "Incapacitated",
+    "Invisible",
+    "Paralyzed",
+    "Petrified",
+    "Poisoned",
+    "Prone",
+    "Restrained",
+    "Stunned",
+    "Unconscious",
+    "Exhaustion"
+]
